a71781ca4f6b21f17b3bd326fe44fe984f86fd35
ThanHenderson <nathan.t.henderson@outlook.com>
2021-07-21 11:33:17 -0500

    update

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const template = document.createElement('template')
 
 template.innerHTML = `
 <title>Artilect</title>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="./index-css.css">
 
 <div class="header">
