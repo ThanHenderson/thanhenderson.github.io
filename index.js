b9582c3ec069ef24edca1a8395b7658d8b12b10c
ThanHenderson <nathan.t.henderson@outlook.com>
2021-07-21 09:40:52 -0500

    update

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ template.innerHTML = `
 			<img src="maple-min.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
 		</a>
 		<ul class="nav list-inline ml-auto ">
-			<li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-			<li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+            <li class="nav-item"><a class="nav-link" href="index.html">About</a></li>
+            <li class="nav-item"><a class="nav-link" href="cv.html">CV</a></li>
+			<li class="nav-item"><a class="nav-link" href="blog.html">Artilect</a></li>
 		</ul>
 	</nav>
 </div>
 `
-document.body.appendChild(template.content)
+document.body.prepend(template.content)
