a757890a96197313ae0911eb20f32e60e4458810
ThanHenderson <nathan.t.henderson@outlook.com>
2021-08-05 09:07:31 -0500

    update

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ template.innerHTML = `
 <title>Artilect</title>
 <link rel="stylesheet" type="text/css" href="./index-css.css">
 
-<div class="header">
-	<nav class="navbar fixed-top navbar-light bg-light">
-		<a class="navbar-brand" href="index.html">
-			<img src="maple-min.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+    <nav>
+		<a href="index.html">
+        	<img src="site_imgs/maple-min.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
 		</a>
-		<ul class="nav list-inline ml-auto ">
-            <li class="nav-item"><a class="nav-link" href="index.html">About</a></li>
-            <!--<li class="nav-item"><a class="nav-link" href="cv.html">CV</a></li>-->
-			<li class="nav-item"><a class="nav-link" href="blog.html">Artilect</a></li>
-		</ul>
-	</nav>
-</div>
+        <ul>
+            <li><a href="index.html">About</a></li>
+            <li><a href="blog.html">Artilect</a></li>
+        </ul>
+    </nav>
 `
 document.body.prepend(template.content)
